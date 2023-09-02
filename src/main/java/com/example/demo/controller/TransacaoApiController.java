9b34ececc1d8545f911e4b989ac191a5fa5b4411
igor-rib-souza <igorrs.rib@gmail.com>
2023-09-01 23:33:54 -0300

    refactor: move logic from controller into service

--- src/main/java/com/example/demo/controller/TransacaoApiController.java
+++ src/main/java/com/example/demo/controller/TransacaoApiController.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.LimiteConta;
 import com.example.demo.model.Transacao;
-import com.example.demo.repository.ContaRepository;
-import com.example.demo.repository.LimiteContaRepository;
-import com.example.demo.repository.TransacaoRepository;
 import com.example.demo.service.TransacaoService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,40 +15,21 @@ public class TransacaoApiController {
 
     @Autowired
     TransacaoService transacaoService;
-    @Autowired
-    TransacaoRepository transacaoRepository;
-    @Autowired
-    ContaRepository contaRepository;
-    @Autowired
-    LimiteContaRepository limiteContaRepository;
 
     @PostMapping(value = "/transacao")
     public ResponseEntity<?> criaTransacao(@RequestBody Transacao transacao){
         try {
-            LimiteConta limiteConta = limiteContaRepository.findById(transacao.getContaId()).get();
-
-
-            if (contaRepository.findById(transacao.getContaId()).isEmpty()){
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("CONTA NÃO ENCONTRADA");
-            }
-
-
-            else if (limiteConta.getValor() < transacao.getValor() && transacao.getTipoOperacaoId() != 4){
-                return ResponseEntity.status(HttpStatus.CONFLICT).body("LIMITE INSUFICIENTE");
-            }
-
-            else if (transacao.getTipoOperacaoId() != 4) {
-                transacao.setValor(transacao.getValor() * -1);
-            }
-
-            limiteConta.updateValor(transacao.getValor());
-            LimiteConta updateLimite = limiteContaRepository.save(limiteConta);
-            Transacao transacaoAux = transacaoRepository.save(transacao);
-                return ResponseEntity.ok(transacaoAux);
+            Transacao transacaoAux = transacaoService.criaTransacao(transacao);
+            return ResponseEntity.ok(transacaoAux);
         }
-
-        catch (Exception e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("CONTA NÃO ENCONTRADA");
+        catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("ERRO INTERNO DO SERVIDOR");
         }
     }
 }
--- src/main/java/com/example/demo/service/TransacaoService.java
+++ src/main/java/com/example/demo/service/TransacaoService.java
@@ -1,6 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.model.LimiteConta;
+import com.example.demo.repository.ContaRepository;
+import com.example.demo.repository.LimiteContaRepository;
 import com.example.demo.repository.TransacaoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.model.Transacao;
@@ -10,10 +14,31 @@ public class TransacaoService {
 
     @Autowired
     TransacaoRepository transacaoRepository;
+    @Autowired
+    ContaRepository contaRepository;
+    @Autowired
+    LimiteContaRepository limiteContaRepository;
 
-    public Transacao criaTransacao(Long contaId, Long tipoOperacaoId, Double valor){
+    public Transacao criaTransacao(Transacao transacao) throws Exception{
 
-        Transacao transacao = new Transacao(contaId, tipoOperacaoId, valor);
+        LimiteConta limiteConta = limiteContaRepository.findById(transacao.getContaId())
+                .orElseThrow(() -> new EntityNotFoundException("CONTA NÃO ENCONTRADA"));
+
+        if (contaRepository.findById(transacao.getContaId()).isEmpty()){
+            throw new EntityNotFoundException("CONTA NÃO ENCONTRADA");
+        }
+
+        if (limiteConta.getValor() < transacao.getValor() && transacao.getTipoOperacaoId() != 4){
+            throw new IllegalArgumentException("LIMITE INSUFICIENTE");
+        }
+
+        if (transacao.getTipoOperacaoId() != 4) {
+            transacao.setValor(transacao.getValor() * -1);
+        }
+
+        limiteConta.updateValor(transacao.getValor());
+        limiteContaRepository.save(limiteConta);
         return transacaoRepository.save(transacao);
     }
 }
+
